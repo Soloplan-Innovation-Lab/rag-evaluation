63ca3e465d24575737a0e022e95bf86cd51c24df
Benjamin Schober <bennischober001@gmail.com>
2024-06-10 20:58:58 +0200

    Add function usage analysis scripts

---
+++ domain-knowledge/notebooks/devexpress/curated_categories.py
@@ -0,0 +1,28 @@
+function_list = {
+    'aggregate_functions': [
+        {
+            "name": "Avg",
+            "description": "Evaluates the average of the values in the collection.",
+            "example": "[Products].Avg([UnitPrice])",
+            "category": "Aggregate Functions",
+            "source": "DevExpress"
+        }
+    ]
+}
+
+
+
+aggregate_functions = [
+    'Avg',
+    'Count',
+    'Exists',
+    'Max',
+    'Min',
+    'Single',
+    'Sum',
+    'InRange',
+]
+
+date_functions = [
+
+]
